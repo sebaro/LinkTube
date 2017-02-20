7f720b0abb7eaba504f8c0c7e745cf706da385a4
sebaro <esebaro@gmail.com>
2017-02-20 17:44:54 +0200

    Update Maxthon extension

--- Maxthon/page.js
+++ Maxthon/page.js
@@ -1,4 +1,4 @@
 javascript:
 var myRuntime = window.external.mxGetRuntime();
 var mxTabs = myRuntime.create('mx.browser.tabs');
-var TabNum = mxTabs.newTab({url:"http://isebaro.com/linktube/?ln=en",activate: true,position:"afterCurrrent"});
+var TabNum = mxTabs.newTab({url:"http://isebaro.com/linktube",activate: true,position:"afterCurrrent"});
